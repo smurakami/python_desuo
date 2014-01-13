706f10b98b1efd5a32e042b074ae312a1b3f16f1
s_murakami <s.murakami@conol.jp>
2014-01-13 15:46:24 +0900

    細かい改善, readmeの作成

--- desuo.py
+++ desuo.py
@@ -10,12 +10,23 @@ def shuffled(origin_string):
         shuffled_string += clone.pop(R.randint(0, len(clone) - 1))
     return shuffled_string
 
+def removeChar(s, c):
+    if c in s:
+        i = s.index(c)
+        s = s[:i] + s[i+1:]
+    return s
+
 if __name__ == "__main__":
     argv = sys.argv
     if len(argv) > 1:
         s = unicode(argv[1])
     else:
-        s = u"祝！新成人めとうございま"
+        s = u"祝！新成人おめでとうございます"
+
+    s = removeChar(s, u"で")
+    s = removeChar(s, u"す")
+    s = removeChar(s, u"お")
+
     s = shuffled(s)
     print s + u"ですお"
 
